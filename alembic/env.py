64c1c10c98539492d9c6650aa4c24f02373c5821
kang9805 <zzionkk@naver.com>
2025-09-09 16:21:17 +0900

    💡 chore: alembic/env.py 모델 및 DB 설정 수정

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,13 @@
 import asyncio
 from logging.config import fileConfig
 
-from myapp.models import Base  # 🚨 실제 프로젝트 모델 Base로 바꿔주세요
-from sqlalchemy.ext.asyncio import create_async_engine
+from app.db.base import Base  # 🚨 실제 프로젝트 모델 Base로 바꿔주세요
+from app.db.session import engine
+
+from sqlalchemy.ext.asyncio import AsyncEngine
 
 from alembic import context
+
 
 # Alembic Config 객체 (alembic.ini 사용)
 config = context.config
@@ -17,11 +20,15 @@ if config.config_file_name is not None:
 target_metadata = Base.metadata
 
 
-def do_run_migrations(connection):
+def do_run_migrations(connection: AsyncEngine):
     """
     실제 migration 실행 로직 (동기 방식)
     """
-    context.configure(connection=connection, target_metadata=target_metadata)
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        dialect_opts={"paramstyle": "named"}
+    )
 
     with context.begin_transaction():
         context.run_migrations()
@@ -31,10 +38,7 @@ async def run_migrations_online():
     """
     비동기 DB 엔진 생성 후 migration 실행
     """
-    connectable = create_async_engine(
-        config.get_main_option("sqlalchemy.url"),
-        future=True,
-    )
+    connectable = engine
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)

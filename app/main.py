a8c41eff727d4af59e344419f49125d052043be0
s4ngmin-9 <s4ngmin9@naver.com>
2025-09-10 17:06:47 +0900

    ✨ feat: diary 라우터 prefix를 /api/v1로 수정

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(
     version="1.0.0"
 )
 
-app.include_router(diary_router, prefix="/api/v1/diaries", tags=["diaries"])
+app.include_router(diary_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():

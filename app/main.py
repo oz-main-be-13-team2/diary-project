5168567df69c8b01761f9942bbe9bd8e4fb18f8a
kang9805 <zzionkk@naver.com>
2025-09-10 16:27:27 +0900

    ✨ feat : main.py에 라우터 포함

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from app.db.base import Base
+from app.db.session import engine
+from app.api.v1 import quote, question, bookmark
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 애플리케이션 시작 시 실행되는 코드
+    print("애플리케이션 시작...")
+    await init_db()
+    yield
+    # 애플리케이션 종료 시 실행되는 코드
+    print("애플리케이션 종료...")
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(quote.router)
+app.include_router(question.router)
+app.include_router(bookmark.router)
+
+
+
+@app.get("/")
+def read_root():
+    return {"message": "명언 및 북마크 API에 오신 것을 환영합니다!"}

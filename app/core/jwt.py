bcffe60f04a658bbb91d8d7965e1e95f8e8f9505
DESKTOP-5JIBLGH\pat76 <pat7654@naver.com>
2025-09-11 10:27:48 +0900

    JWT 유틸

---
+++ app/core/jwt.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+
+from jose import jwt
+
+SECRET_KEY = "secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (
+        expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)

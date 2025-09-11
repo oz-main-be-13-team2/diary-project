949554181fc2cebad680a69e3b633dbd39c5c855
DESKTOP-5JIBLGH\pat76 <pat7654@naver.com>
2025-09-11 10:25:29 +0900

    비밀번호 해싱

--- app/core/security.py
+++ app/core/security.py
@@ -1,11 +1,4 @@
-from datetime import datetime, timedelta
-
-from jose import jwt
 from passlib.context import CryptContext
-
-SECRET_KEY = "YOUR_SECRET_KEY"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -16,12 +9,3 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + (
-        expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    )
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)

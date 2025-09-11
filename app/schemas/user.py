912524abf26bcd96d8ff6515c8203bf94e5f7415
DESKTOP-5JIBLGH\pat76 <pat7654@naver.com>
2025-09-11 10:24:25 +0900

    Pydantic 스키마

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -16,17 +16,16 @@ class UserSignupResponse(BaseModel):
     nickname: Optional[str] = None
     created_at: datetime
 
+    class Config:
+        orm_mode = True
+
+
+class UserLoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
 
 class TokenResponse(BaseModel):
     access_token: str
+    refresh_token: Optional[str] = None
     token_type: str = "bearer"
-
-
-class UserResponse(BaseModel):
-    id: int
-    email: EmailStr
-    nickname: Optional[str] = None
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
